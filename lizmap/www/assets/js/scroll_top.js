349b9b98ec40df3faba9cf6b96eb4f6a297123a4
ComuneMontelupoFiorentino <e.gulli@comune.montelupo-fiorentino.fi.it>
2025-07-23 13:42:57 +0200

    Create scroll_top.js
    Add a JS function

---
+++ lizmap/www/assets/js/scroll_top.js
@@ -0,0 +1,19 @@
+// Get the button
+let scrollUpBtn = document.getElementById("scrollTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollUpBtn.style.display = "block";
+  } else {
+    scrollUpBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
